e31943ea8d83296357c087b5e955b0691ae40f24
#Brocode# <energypanashemugabe@gmail.com>
2023-11-07 08:08:12 +0200

    added some text to the header and added a footer

--- navbar/javascript/main.js
+++ navbar/javascript/main.js
@@ -1,10 +1,22 @@
 window.addEventListener('scroll', function() {
-    var navbar = document.querySelector('.navbar');
-    var scrolled = window.scrollY > 0;
-    
-    if (scrolled) {
-      navbar.classList.add('scrolled');
-    } else {
-      navbar.classList.remove('scrolled');
-    }
-  });
+  var navbar = document.querySelector('.navbar');
+  var scrolled = window.scrollY > 0;
+  if (scrolled) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
+
+
+document.querySelector('.action-btn').addEventListener('click',function(e){
+  e.preventDefault();
+  e.stopPropagation();
+  document.querySelector('.logging').classList.toggle('hidden');
+})
+
+window.addEventListener('click',function(e){
+  if(e.target!==document.querySelector('.logging')){
+    document.querySelector('.logging').classList.add('hidden');
+  }
+})
